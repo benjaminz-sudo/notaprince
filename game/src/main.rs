01ee50a79a46b08bc2f885f8cf1a2d2c6f94021e
melissatefit-e4inf <melissa.tefit@edu.esiee.fr>
2026-03-25 23:09:26 +0100

    configuration de  20 salles avec IDs uniques + test de verification de la generation des salles (affichage des id)

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+use rand::Rng; // usage de la crate rand pour la génération de nombres aléatoires
 use std::collections::BTreeMap;
 use std::io;
 
@@ -30,4 +30,23 @@ impl Room {
 fn main() {
     //Création du BTreeMap
     let mut monde: BTreeMap<u32, Room> = BTreeMap::new();
+    let mut rng = rand::thread_rng();
+
+    // 20 salles avec IDs uniques
+    while monde.len() < 20 {
+        // ID entre 1 et 100
+        let id_aleatoire: u32 = rng.gen_range(1..101);
+
+        // verification que l'ID est unique pour ne pas écraser une salle
+        if !monde.contains_key(&id_aleatoire) {
+            let nom = format!("Salle mystère n°{}", id_aleatoire);
+            let nouvelle_salle = Room::new(nom);
+            monde.insert(id_aleatoire, nouvelle_salle);
+        }
+    }
+
+    println!("\nListe des salles dans le BTreeMap :");
+    for (id, salle) in &monde {
+        println!(" - ID [{}]: {}", id, salle.get_description());
+    }
 }
